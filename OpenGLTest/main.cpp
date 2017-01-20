0306659a7948022e02aa0cc8cd0a1d4223b40b96
yuanyi <603035634@qq.com>
2017-01-20 15:42:34 +0800

    Example 1.

--- OpenGLTest/main.cpp
+++ OpenGLTest/main.cpp
@@ -1,52 +1,63 @@
-#include <math.h>
-#include <GL/glut.h>
-#include <stdio.h>
+#include <gl/glut.h>
+#include <Windows.h>
 
-const GLfloat Pi = 3.1415926536f;
-void myDisplay(void)
-{
-	GLfloat a = 1 / (2 - 2 * cos(72 * Pi / 180));
-	GLfloat bx = a * cos(18 * Pi / 180);
-	GLfloat by = a * sin(18 * Pi / 180);
-	GLfloat cy = -a * cos(18 * Pi / 180);
-	GLfloat
-		PointA[2] = { 0, a },
-		PointB[2] = { bx, by },
-		PointC[2] = { 0.5, cy },
-		PointD[2] = { -0.5, cy },
-		PointE[2] = { -bx, by };
+static GLfloat spin = 0.0;
 
+void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
-	// 按照A->C->E->B->D->A的顺序，可以一笔将五角星画出
-	glBegin(GL_LINE_LOOP);
-	glVertex2fv(PointA);
-	glVertex2fv(PointC);
-	glVertex2fv(PointE);
-	glVertex2fv(PointB);
-	glVertex2fv(PointD);
-	glEnd();
-	glFlush();
+	glPushMatrix();
+	glRotatef(spin, 0, 0, 1.0f);
+	glColor3f(0.5, 0.5, 0.5);
+	glRectf(-25, -25, 25, 25);
+	glPopMatrix();
+	glutSwapBuffers();
 }
 
-int main(int argc, char *argv[])
+void spinDisplay(void) {
+	spin += 2.0;
+	if (spin > 360) {
+		spin -= 360;
+	}
+	Sleep((int)((float)1000 / (float)60));
+	glutPostRedisplay();
+}
 
-{
-	puts("start");
+void mouse(int button, int state, int x, int y) {
+	switch (button) {
+	case GLUT_LEFT_BUTTON:
+		if (state == GLUT_DOWN) {
+			glutIdleFunc(spinDisplay);
+		}
+		break;
+	case GLUT_RIGHT_BUTTON:
+		if (state == GLUT_DOWN) {
+			glutIdleFunc(NULL);
+		}
+		break;
+	default:
+		break;
+	}
 
+}
+
+int main(int argc, char* argv[]) {
 	glutInit(&argc, argv);
-
-	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(250, 250);
 	glutInitWindowPosition(100, 100);
+	glutCreateWindow("hello opengl");
 
-	glutInitWindowSize(400, 400);
+	glClearColor(0, 0, 0, 0);
+	glShadeModel(GL_FLAT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-50, 50, -50, 50, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 
-	glutCreateWindow("第一个OpenGL程序");
-
-	glutDisplayFunc(&myDisplay);
-
+	glutDisplayFunc(display);
+	glutMouseFunc(mouse);
 	glutMainLoop();
 
 	return 0;
-
 }
